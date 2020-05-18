6358384d0c11507162db0cec8ed476fe4aaad993
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-05-18 03:17:15 -0400

    added hamburger for mobile menu

---
+++ portfolio/js/main.js
@@ -0,0 +1,42 @@
+//toggle the menu when on mobile device (when hamburger button is clicked)
+function toggleMenu(){
+    var mobileNavBar = document.getElementById("mobileNavBar");
+    var items = mobileNavBar.getElementsByClassName("navBarItems");
+    var length = items.length;
+    if(mobileNavBar.style.display === "block" ){
+        $("#mobileNavBar").animate({
+           opacity : "0"
+        },500,function(){            
+            mobileNavBar.style.display = "none";
+        });
+        //change the icon abck to hamburger on closing the menu 
+        $("#hamburger").attr('src','../icons/hamburger.png')
+    }
+    else{
+        //change the icon of the menu toggler
+        $("#hamburger").attr('src','../icons/close.png')
+        mobileNavBar.style.display = "block"
+        $("#mobileNavBar").animate({
+           opacity : "1"
+        },500)
+        for(var i=0;i<length;i++){
+            items[i].style.display = "block";
+            items[i].style.borderBottom = "0.5px solid white";
+        }
+    }
+    
+}
+
+function checkDeviceWidth(width) {
+  if (width.matches) { // If media query matches
+    document.getElementById("mobileNav").style.display = "block";
+    document.getElementById("mobileNavBar").style.display = "none";
+  } else {
+    document.getElementById("mobileNav").style.display = "none";
+    document.getElementById("mobileNavBar").style.display = "none";
+  }
+}
+
+var width = window.matchMedia("(max-width: 600px)")
+checkDeviceWidth(width) // Call listener function at run time
+width.addListener(checkDeviceWidth) // Attach listener function on state changes
